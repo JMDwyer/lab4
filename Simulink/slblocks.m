fb5024728f926efd0c643a80cc7f84abafc950cf
Peter Slater <pas324@cornell.edu>
2017-05-01 18:42:39 -0400

    Got 2D simulation working

---
+++ Simulink/slblocks.m
@@ -0,0 +1,12 @@
+function blkStruct = slblocks
+    addpath('./Library');
+    blkStruct.Browser(1).Library = 'ALD1106';
+    blkStruct.Browser(1).Name = 'ALD1106 (NFET)';
+    blkStruct.Browser(2).Library = 'ALD1107';
+    blkStruct.Browser(2).Name = 'ALD1107 (PFET)';
+    blkStruct.Browser(3).Library = 'ALD1105';
+    blkStruct.Browser(3).Name = 'ALD1105';
+    blkStruct.Browser(4).Library = 'SimscapeMacros';
+    blkStruct.Browser(4).Name = 'Simscape Macros';
+    blkStruct.Browser(5).Library = 'ProjectBlocks';
+    blkStruct.Browser(5).Name = 'Project Blocks';
